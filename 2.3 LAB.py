89b3062959c78caa7d6763c222ddc8261b32b824
skyeoh06 <61610325+skyeoh06@users.noreply.github.com>
2024-05-14 20:44:27 +0800

    Update 2.3 LAB.py

--- 2.3 LAB.py
+++ 2.3 LAB.py
@@ -63,3 +63,54 @@ print(6. // 3.)
 2.0
 2.0
 2.0
+
+print(6 // 4)
+print(6. // 4)
+>>
+1
+1.0
+
+# 1.5
+# Operators: remainder (modulo)
+# The result of the operator is a remainder left after the integer division.
+print(14 % 4)
+>>
+2
+# the result is two. This is why:
+
+# 14 // 4 gives 3 → this is the integer quotient;
+# 3 * 4 gives 12 → as a result of quotient and divisor multiplication;
+# 14 - 12 gives 2 → this is the remainder.
+
+print(12 % 4.5)
+>>
+3.0
+
+# 1.6
+# Operators: addition
+print(-4 + 4)
+print(-4. + 8)
+>>
+0
+4.0
+
+# The subtraction operator, unary and binary operators
+print(-4 - 4)
+print(4. - 8)
+print(-1.1)
+print(+2)
+>>
+-8
+-4.0
+-1.1
+2
+
+# 1.8
+# Operators and their bindings: exponentiation
+print(2 ** 2 ** 3)
+>>
+256
+
+# 2 ** 3 → 8; 2 ** 8 → 256
+# the exponentiation operator uses right-sided binding.
+
