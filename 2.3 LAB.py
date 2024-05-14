86238558ee3fb3ff78f0ecd884670c032b01ab48
skyeoh06 <61610325+skyeoh06@users.noreply.github.com>
2024-05-14 20:23:48 +0800

    Create 2.3 LAB.py

---
+++ 2.3 LAB.py
@@ -0,0 +1,65 @@
+# 1.1 
+# Operators - data manipulation tools
+print(2+2)
+print(2-2)
+print(2*2)
+print(2/2)
+>>
+4
+0
+4
+1.0
+
+# 1.2
+# Arithmetic operators: exponentiation
+# A ** (double asterisk) sign is an exponentiation (power) operator. Its left argument is the base, its right, the exponent.
+print(2 ** 3)
+print(2 ** 3.)
+print(2. ** 3)
+print(2. ** 3.)
+>>
+8
+8.0
+8.0
+8.0
+
+# 1.3
+# Arithmetic operators: multiplication
+# An * (asterisk) sign is a multiplication operator.
+print(2 * 3)
+print(2 * 3.)
+print(2. * 3)
+print(2. * 3.)
+>> 
+6
+6.0
+6.0
+6.0
+
+# Arithmetic operators: division
+# A / (slash) sign is a divisional operator.
+
+# The value in front of the slash is a dividend, the value behind the slash, a divisor.
+# The result produced by the division operator is always a float,
+print(6 / 3)
+print(6 / 3.)
+print(6. / 3)
+print(6. / 3.)
+>>
+2.0
+2.0
+2.0
+2.0
+
+# 1.4
+# Arithmetic operators: integer division
+# A // (double slash) sign is an integer divisional operator. 
+print(6 // 3)
+print(6 // 3.)
+print(6. // 3)
+print(6. // 3.)
+>>
+2
+2.0
+2.0
+2.0
