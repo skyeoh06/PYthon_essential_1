2ab43739d14570dc14e4ed57673c4580baf4cddc
skyeoh06 <61610325+skyeoh06@users.noreply.github.com>
2024-05-14 16:54:30 +0800

    Update 4.3.1.8 LAB.py

--- 4.3.1.8 LAB.py
+++ 4.3.1.8 LAB.py
@@ -1,10 +1,10 @@
 # Objectives
 # Familiarize the student with:
-
 # projecting and writing parameterized functions;
 # utilizing the return statement;
 # building a set of utility functions;
 # utilizing the student's own functions.
+
 # Scenario
 # Your task is to write and test a function which takes three arguments (a year, a month, and a day of the month) and returns the corresponding day of the year, or returns None if any of the arguments is invalid.
 
