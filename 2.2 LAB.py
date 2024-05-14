befa2937bc209f188abeb98f9a7081aaba8d0ced
skyeoh06 <61610325+skyeoh06@users.noreply.github.com>
2024-05-14 19:53:15 +0800

    Create 2.2 LAB.py

---
+++ 2.2 LAB.py
@@ -0,0 +1,42 @@
+# Python Literals
+# 1.2
+print("2")
+print(2)
+>>
+2
+2
+
+# 1.3
+# Integers: octal and hexadecimal numbers
+print(0o123)
+>> 83
+print(0x123)
+>> 291
+
+# 1.7
+# coding float
+print(0.0000000000000000000001)
+>> 1e-22
+
+# 1.9
+print('I\'m Monty Python.')
+>> I'm Monty Python.
+print("I'm Monty Python.")
+>> I'm Monty Python.
+
+# 1.11
+# Objectives
+# becoming familiar with the print() function and its formatting capabilities;
+# practicing coding strings;
+# experimenting with Python code.
+# Scenario
+# Write a one-line piece of code, using the print() function, as well as the newline and escape characters, to match the expected result outputted on three lines.
+
+# Expected output
+
+# "I'm"
+# ""learning""
+# """Python"""
+print('"I\'m"\n""learning""\n"""Python"""')
+
+#
