7ef9867eef25fdae2f6f74377802bbc97f322d48
skyeoh06 <61610325+skyeoh06@users.noreply.github.com>
2024-05-15 17:22:03 +0800

    Create 3.4.1.12.py

---
+++ 3.4.1.12.py
@@ -0,0 +1,19 @@
+# Lists in action
+# 1
+my_list = [10, 1, 8, 3, 5]
+
+my_list[0], my_list[4] = my_list[4], my_list[0]
+my_list[1], my_list[3] = my_list[3], my_list[1]
+
+print(my_list)
+>> [5, 3, 8, 1, 10]
+
+# 2
+my_list = [10, 1, 8, 3, 5]
+length = len(my_list)
+
+for i in range(length // 2):
+    my_list[i], my_list[length - i - 1] = my_list[length - i - 1], my_list[i]
+
+print(my_list)
+>> [5, 3, 8, 1, 10]
