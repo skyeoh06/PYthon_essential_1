fbcdfaf76be80a5e005029bd6ae49f9bc2c20256
skyeoh06 <61610325+skyeoh06@users.noreply.github.com>
2024-05-15 17:20:14 +0800

    Update 3.4 LAB.py

--- 3.4 LAB.py
+++ 3.4 LAB.py
@@ -81,4 +81,79 @@ print(numbers[-2])
 1
 2
 
+# 1.8
+# Adding elements to a list: append() and insert()
+numbers = [111, 7, 2, 1]
+print(len(numbers))
+print(numbers)
+
+###
+
+numbers.append(4)
+
+print(len(numbers))
+print(numbers)
+
+###
+
+numbers.insert(0, 222)
+print(len(numbers))
+print(numbers)
+
+#
+>>
+4
+[111, 7, 2, 1]
+5
+[111, 7, 2, 1, 4]
+6
+[222, 111, 7, 2, 1, 4]
+
+# 1.9
+my_list = []  # Creating an empty list.
+
+for i in range(5):
+    my_list.append(i + 1)
+
+print(my_list)
+>> [1, 2, 3, 4, 5]
+
+# 1.10
+# Making use of lis
+my_list = [10, 1, 8, 3, 5]
+total = 0
+
+for i in range(len(my_list)):
+    total += my_list[i]
+
+print(total)
+>> 27
+
+# 1.11
+# Lists in action
+variable_1 = 1
+variable_2 = 2
+
+variable_2 = variable_1
+variable_1 = variable_2
+print("condition 1: ",variable_1,variable_2)
+
+variable_1 = 1
+variable_2 = 2
+
+auxiliary = variable_1
+variable_1 = variable_2
+variable_2 = auxiliary
+print("condition 2: ",variable_1,variable_2)
+
+variable_1 = 1
+variable_2 = 2
+
+variable_1, variable_2 = variable_2, variable_1
+print("condition 3: ",variable_1,variable_2)
+>>
+condition 1:  1 1
+condition 2:  2 1
+condition 3:  2 1
+
 
