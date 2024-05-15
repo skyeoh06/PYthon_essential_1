c5870f080b620d192f7d5be4142b0e9581d1e4fe
skyeoh06 <61610325+skyeoh06@users.noreply.github.com>
2024-05-15 14:46:03 +0800

    Update 3.2 LAB.py

--- 3.2 LAB.py
+++ 3.2 LAB.py
@@ -24,3 +24,52 @@ Inside the loop. 3
 Inside the loop. 2
 Inside the loop. 1
 Outside the loop. 0
+
+# For Loop
+for i in range(2, 8, 3):
+    print("The value of i is currently", i)
+>>
+The value of i is currently 2
+The value of i is currently 5
+
+# 1.6
+# Scenario
+# Your task is very simple here: write a program that uses a for loop to "count mississippily" to five. Having counted to five, the program should print to the screen the final message "Ready or not, here I come!"
+
+# Use the skeleton we've provided in the editor.
+
+# EXTRA INFO
+
+# Note that the code in the editor contains two elements which may not be fully clear to you at this moment: the import time statement, and the sleep() method. We're going to talk about them soon.
+
+# For the time being, we'd just like you to know that we've imported the time module and used the sleep() method to suspend the execution of each subsequent print() function inside the for loop for one second, 
+# so that the message outputted to the console resembles an actual counting. Don't worry - you'll soon learn more about modules and methods.
+
+# Expected output
+# 1 Mississippi
+# 2 Mississippi
+# 3 Mississippi
+# 4 Mississippi
+# 5 Mississippi
+# Ready or not, here I come!
+
+import time
+
+# Write a for loop that counts to five.
+    # Body of the loop - print the loop iteration number and the word "Mississippi".
+    # Body of the loop - use: time.sleep(1)
+for n in range (6):
+    print(n, "Mississippi")
+    time.sleep(1)
+
+# Write a print function with the final message.
+print("Ready or not, here I come!")
+>>
+0 Mississippi
+1 Mississippi
+2 Mississippi
+3 Mississippi
+4 Mississippi
+5 Mississippi
+Ready or not, here I come!
+
