a9069920c5e730ad2ef4d7579dca489dcab7a297
skyeoh06 <61610325+skyeoh06@users.noreply.github.com>
2024-05-15 13:09:49 +0800

    Create 2.6.1.11 LAB.py

---
+++ 2.6.1.11 LAB.py
@@ -0,0 +1,55 @@
+# Scenario
+# Your task is to prepare a simple code able to evaluate the end time of a period of time, given as a number of minutes (it could be arbitrarily large). The start time is given as a pair of hours (0..23) and minutes (0..59). The result has to be printed to the console.
+
+# For example, if an event starts at 12:17 and lasts 59 minutes, it will end at 13:16.
+
+# Don't worry about any imperfections in your code - it's okay if it accepts an invalid time - the most important thing is that the code produce valid results for valid input data.
+
+# Test your code carefully. Hint: using the % operator may be the key to success.
+
+# Test Data
+# Sample input:
+# 12
+# 17
+# 59
+
+# Expected output: 13:16
+
+
+# Sample input:
+# 23
+# 58
+# 642
+
+# Expected output: 10:40
+
+
+# Sample input:
+# 0
+# 1
+# 2939
+
+# Expected output: 1:0
+
+hour = int(input("Starting time (hours): "))
+mins = int(input("Starting time (minutes): "))
+dura = int(input("Event duration (minutes): "))
+
+# Write your code here.
+hour = (hour + ((dura+mins)//60))%24
+mins = (dura+mins)%60
+print(hour,":",mins)
+
+>>
+Starting time (hours): 12
+Starting time (minutes): 17
+Event duration (minutes): 59
+13 : 16
+Starting time (hours): 23
+Starting time (minutes): 58
+Event duration (minutes): 642
+10 : 40
+Starting time (hours): 0
+Starting time (minutes): 1
+Event duration (minutes): 2939
+1 : 0
