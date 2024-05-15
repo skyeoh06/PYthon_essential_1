84aa5c24c85caec1ced1797e5b0f493384f38a08
skyeoh06 <61610325+skyeoh06@users.noreply.github.com>
2024-05-15 10:36:42 +0800

    Create 2.6.1.9 LAB.py

---
+++ 2.6.1.9 LAB.py
@@ -0,0 +1,36 @@
+# Scenario
+# Your task is to complete the code in order to evaluate the results of four basic arithmetic operations.
+
+# The results have to be printed to the console.
+
+# You may not be able to protect the code from a user who wants to divide by zero. That's okay, don't worry about it for now.
+
+# Test your code - does it produce the results you expect?
+
+# input a float value for variable a here
+a=float(input("Please enter the first number: "))
+# input a float value for variable b here
+b=float(input("Please enter the second number: "))
+
+# output the result of addition here
+print("addition: ",a+b)
+# output the result of subtraction here
+print("subtraction: ",a-b)
+# output the result of multiplication here
+print("multiplication: ",a*b)
+# output the result of division here
+print("division: ",a/b)
+
+print("\nThat's all, folks!")
+>>
+Please enter the first number: 29
+Please enter the second number: 15
+addition:  44.0
+subtraction:  14.0
+multiplication:  435.0
+division:  1.9333333333333333
+
+That's all, folks!
+
+
+
